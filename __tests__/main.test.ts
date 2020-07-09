a294c57b8bc988b225cb8add59cba8eba95e1967
imcuttle <moyuyc95@gmail.com>
2020-07-09 14:40:07 +0800

    chore: inital commit

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,10 +3,34 @@
  * @author imcuttle
  * @date 2018/4/4
  */
-const assignRef = require('../')
+import assignRef from '../'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 describe('assignRef', function () {
-  it('should', function (done) {
-    done()
+  beforeEach(() => {
+    document.body.innerHTML = ''
+
+    const div = document.createElement('div')
+    div.id = 'root'
+    document.body.appendChild(div)
+  })
+
+  it('should works', function () {
+    const refObj = React.createRef()
+    let ref = null
+    const refCallback = (instance) => (ref = instance)
+
+    const elem = React.createElement('div', {
+      ref: (instance) => {
+        assignRef(refObj, instance)
+        assignRef(refCallback, instance)
+      }
+    })
+    ReactDOM.render(elem, document.getElementById('root'))
+
+    expect(ref).toBe(refObj.current)
+
+    expect(ref).toBe(document.getElementById('root').firstChild)
   })
 })
